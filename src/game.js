f62d33579fda85cc5481fccb758f3c9b646408cd
Stierlitz <isaevmaxim2010@gmail.com>
2021-02-21 14:12:26 +0300

    Буксуем в попытках. Поменять вопрос.

--- src/game.js
+++ src/game.js
@@ -1,38 +1,39 @@
-const myQuestions = [
-    {
-        question: 'Мой вопрос 1',
-        answer: ['0', '1', '2', '3'],
-        rightAnswer: 3
-    },
-    {
-        question: 'Мой вопрос 2',
-        answer: ['0', '1', '2', '3'],
-        rightAnswer: 3
-    },
-    {
-        question: 'Мой вопрос 3',
-        answer: ['0', '1', '2', '3'],
-        rightAnswer: 3
-    },
-    {
-        question: 'Мой вопрос',
-        answer: ['0', '1', '2', '3'],
-        rightAnswer: 3
-    },
-    {
-        question: 'Мой вопрос',
-        answer: ['0', '1', '2', '3'],
-        rightAnswer: 3
-    },
-]
-
 export class Game {
     constructor() {
         this.renderStartButton();
     }
+        myQuestions = [
+        {
+            question: 'Мой вопрос 1',
+            answer: ['0', '1', '2', '3'],
+            rightAnswer: 3
+        },
+        {
+            question: 'Мой вопрос 2',
+            answer: ['0', '1', '2', '3'],
+            rightAnswer: 3
+        },
+        {
+            question: 'Мой вопрос 3',
+            answer: ['0', '1', '2', '3'],
+            rightAnswer: 3
+        },
+        {
+            question: 'Мой вопрос 4',
+            answer: ['0', '1', '2', '3'],
+            rightAnswer: 3
+        },
+        {
+            question: 'Мой вопрос 5',
+            answer: ['0', '1', '2', '3'],
+            rightAnswer: 3
+        },
+    ]
+
+        i = 0;
 
     renderStartButton() {
-        const startButton = document.createElement('button')
+        const startButton = document.createElement('button');
         startButton.innerText = 'СТАРТ';
         startButton.addEventListener('click', () => {
             this.start();
@@ -41,25 +42,51 @@ export class Game {
     }
 
     start() {
-        const showQuestion = document.createElement('div')
-        showQuestion.innerText = myQuestions[0].question;
+        let showQuestion = document.createElement('div');
+        showQuestion.innerText = this.myQuestions[this.i].question;
         document.querySelector('.startButton').append(showQuestion);
 
-        let showAnswer = document.createElement('div')
-        // showAnswer.setAttribute('type','radio')
-        showAnswer.innerText = myQuestions[0].answer;
-        document.querySelector('.startButton').append(showAnswer);
+        const acceptButton = document.createElement('button')
+        acceptButton.innerText = 'Подтвердить';
+        acceptButton.addEventListener('click',() => {
+            this.accept();
+        })
+        document.querySelector('.acceptButton').append(acceptButton);
+    }
 
-        // const acceptButton = document.createElement('button')
-        // acceptButton.innerText = 'Подтвердить';
-        // acceptButton.addEventListener('click',() => {
-        //     this.accept();
-        // })
-        // document.querySelector('.acceptButton').append(acceptButton);
+    nextQuestion() {
+        let nextQuestion = document.createElement('div');
+        nextQuestion.innerHTML = this.myQuestions[this.i].question;
+        document.querySelector('.startButton').append(nextQuestion);
+    }
 
+    accept() {
+        this.i = i + 1;
+        this.nextQuestion();
+        i++;
 
     }
 }
+
+// const showAnswer1 = document.createElement('input');
+// showAnswer1.setAttribute('type','radio');
+// // showAnswer1.innerText = this.myQuestions[0].answer;
+// document.querySelector('.startButton').append(showAnswer1);
+//
+// const showAnswer2 = document.createElement('input');
+// showAnswer2.setAttribute('type','radio');
+// // showAnswer2.innerText = this.myQuestions[0].answer;
+// document.querySelector('.startButton').append(showAnswer2);
+//
+// const showAnswer3 = document.createElement('input');
+// showAnswer3.setAttribute('type','radio');
+// // showAnswer3.innerText = this.myQuestions[0].answer;
+// document.querySelector('.startButton').append(showAnswer3);
+//
+// const showAnswer4 = document.createElement('input');
+// showAnswer4.setAttribute('type','radio');
+// // showAnswer4.innerText = this.myQuestions[0].answer;
+// document.querySelector('.startButton').append(showAnswer4);
 
 
 
